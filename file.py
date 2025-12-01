d84428fa700ad815c76add0308a6a322882970f2
Barbara Gorska Sennerstam <barbara@sennerstam.com>
2025-12-01 15:47:12 +0100

    changed file

--- file.py
+++ file.py
@@ -1,1 +1,10 @@
 x=5
+
+for i in range(10):
+    x = x + i
+print(x)
+# This code initializes x to 5 and then iterates from 0 to 9, adding each value of i to x.
+# After the loop, it prints the final value of x, which will be 50.
+# Final value of x will be 5 + (0+1+2+3+4+5+6+7+8+9) = 5 + 45 = 50  
+
+# Output: 50
